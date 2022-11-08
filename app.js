18539313ff663067bc36ff6540aa65345a76d1e3
Aleksandra Tomasiewicz <s20777@pjwstk.edu.pl>
2022-11-08 15:40:52 +0100

    TIN display image and add controllers

--- app.js
+++ app.js
@@ -46,11 +46,6 @@ app.use(function(err, req, res, next) {
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
-// App
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
-
 app.listen(PORT, HOST, () => {
   console.log(`Running on http://${HOST}:${PORT}`);
 });
