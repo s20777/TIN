0ea08a20e1b33c955102173b46f16cc7fab4640f
Aleksandra Tomasiewicz <s20777@pjwstk.edu.pl>
2022-11-25 22:11:54 +0100

    TIN add department view

--- controllers/departamentController.js
+++ controllers/departamentController.js
@@ -1,23 +1,52 @@
+const DepartmentRepository = require('../repository/sequelize/departmentRepository')
+
 exports.showDepartmentList = (req, res, next) => {
-    res.render('pages/department/list', { navLocation : "dept"});
+    DepartmentRepository.getDepartments()
+        .then(depts => {
+            res.render('pages/department/list', {
+                depts: depts,
+                navLocation : "dept"});
+        })
+
 }
 
-exports.showDepartmentForm = (req, res, next) => {
+exports.showAddDepartmentForm = (req, res, next) => {
     res.render('pages/department/form', {
         navLocation: 'dept',
-        user: {},
+        dept: {},
         pageTitle: 'Nowy departament',
         formMode: 'createNew',
         btnLabel: 'Dodaj departament',
-        formAction: '/dept/add',
+        formAction: '/departments/add',
         validationErrors: []
     });
 }
 
-exports.showDepartmentDetails = (req, res, next) => {
-    res.render('pages/department/details', {navLocation : "dept"});
+exports.showEditDepartmentForm = (req, res, next) => {
+    const deptId = req.params.deptId;
+
+    DepartmentRepository.getDepartmentById(deptId)
+        .then(dept => {
+            res.render('pages/department/form', {
+                dept: dept,
+                formMode: 'edit',
+                pageTitle: 'Edycja departamentu',
+                btnLabel: 'Edytuj departament',
+                formAction: '/departments/edit',
+                navLocation: 'dept'
+            })})
 }
 
-exports.showDepartmentEdit = (req, res, next) => {
-    res.render('pages/department/form-edit', {navLocation : "dept"});
+exports.showDepartmentDetails = (req, res, next) => {
+    const deptId = req.params.deptId;
+
+    DepartmentRepository.getDepartmentById(deptId)
+        .then(dept => {
+            res.render('pages/department/form', {
+                dept: dept,
+                formMode: 'showDetails',
+                pageTitle: 'Szczegóły departamentu',
+                formAction: '',
+                navLocation: 'dept'
+            })})
 }
--- routes/departmentRoute.js
+++ routes/departmentRoute.js
@@ -4,9 +4,9 @@ const router = express.Router();
 const departmentController = require('../controllers/departamentController');
 
 router.get('/', departmentController.showDepartmentList);
-router.get('/add', departmentController.showDepartmentForm);
+router.get('/add', departmentController.showAddDepartmentForm);
+router.get('/edit/:deptId', departmentController.showEditDepartmentForm);
 router.get('/details/:deptId', departmentController.showDepartmentDetails);
-router.get('/edit/:deptId', departmentController.showDepartmentEdit);
 
 
 module.exports = router;
